86b410c1d35387de3d10d1ea41f9de59b61cab99
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2025-02-11 08:32:33 +0100

    Some numbers and memory considerations

--- course2-c++/main.c
+++ course2-c++/main.c
@@ -15,23 +15,26 @@ int main(){
     int i = 0;
     printf("\nCiao per: %d", i);
     float f = 0.8997;
-    printf("Ciao ciao ciao! Float: %f", f);
+    printf("\nCiao ciao ciao! Float: %f", f);
     bool flag = true;
-    printf("Bool: %s", flag ? "Vero":"falso");
+    printf("\nBool: %s", flag ? "Vero":"falso");
     printf("\nHello!");
 
     //pointers
     int* pointer = &i;
-    printf("Pointer: %d", *pointer);
+    printf("\nPointer: %d", *pointer);
 
     //arrays:
     float array_float[1];
 
 
 
-    char string[] = "Hello!\0"; // \0 terminator char
-    printf("String: %s", string);
+    char string[] = "Hello There!"; // \0 terminator char
+    printf("\nString: %s, dimensione: %d", string, sizeof(string));
 
+    double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089; //ca 20 cifre decimali
+
+    printf("\nPI: %.1000f, dimensione: %d", pi, sizeof(pi));
     return 0;
 
 }
